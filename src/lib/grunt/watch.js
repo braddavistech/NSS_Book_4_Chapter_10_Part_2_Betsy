407666a4b1cac57c8c8a5e823511c61a1c4ba056
braddavistech <brad@braddavistech.com>
2018-10-22 21:26:46 -0500

    grunt change since local didn't work

--- src/lib/grunt/watch.js
+++ src/lib/grunt/watch.js
@@ -1,6 +1,6 @@
 module.exports = {
   scripts: {
-      files: ["./index.html", "./scripts/**/*.js", "./styles/**/*.css", "!node_modules/**/*.js"],
+      files: ["./index.html", "./scripts/*.js", "./styles/**/*.css", "!node_modules/**/*.js"],
       tasks: ["eslint", "browserify", "uglify", "copy"],
       options: {
           spawn: false,
